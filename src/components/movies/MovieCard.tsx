b1088b04f7ab34752c177c3aa355f6dcd5a2f9ff
rohit0805 <rohit5822540@gmail.com>
2024-07-03 00:30:03 +0530

    Add lazyloading image

--- src/components/movies/MovieCard.tsx
+++ src/components/movies/MovieCard.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "./movieDisplay.css";
 import { imageURL } from "../../util";
+import {LazyLoadImage} from 'react-lazy-load-image-component';
 
 const MovieCard = ({ imagePath, title, rating, measureRef = null }) => {
   return (
     <div className="movieCardContainer" ref={measureRef}>
-      <img src={`${imageURL}${imagePath}`} />
+      <LazyLoadImage src={`${imageURL}${imagePath}`} width={200} height={300}/>
       <div className="movieCardInfo">
         <p>{title}</p>
         <p>{rating}</p>
